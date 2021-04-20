8b1bf78fd669692bec5e70f6a2c1af379016b65d
DiegoE14 <diegogeg21@gmail.com>
2021-04-20 18:11:30 -0500

    update cars

--- automobile/models.py
+++ automobile/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Auto(models.Model):
+    marca=models.CharField(max_length=150, null=False)
+    modelo=models.IntegerField(null=False)
+    color=models.CharField(max_length=100, null=False)
